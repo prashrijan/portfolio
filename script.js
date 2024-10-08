8287fb2803eb245cc704287511054bc80e5050af
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-08 15:06:04 +1100

    hero section completed

--- script.js
+++ script.js
@@ -1,37 +1,64 @@
-// let tl = gsap.timeline()
+let tl = gsap.timeline()
 
-// tl.from(".text", {
-//     y: 30,
-//     opacity: 0,
-//     ease: "power2.inOut",
-//     delay: 1,
-//     duration: 1
-// })
+tl.from(".text", {
+    y: 30,
+    opacity: 0,
+    ease: "power2.inOut",
+    delay: 1,
+    duration: 1
+})
 
-// tl.from(".loader", {
-//     width: 0,
-//     ease: "power4.inOut",
-//     duration: 1
-// })
+tl.from(".loader", {
+    width: 0,
+    ease: "power4.inOut",
+    duration: 1
+})
 
-// tl.to('.pre-loader', {
-//     top: "-100%",
-//     ease: "power4.inOut",
-//     duration: 1
-// })
+tl.to('.pre-loader', {
+    top: "-100%",
+    ease: "power4.inOut",
+    duration: 1
+})
 
 
-// tl.from('.logo span', {
-//     y: -60,
-//     duration: 1,
-//     ease: "power2.inOut",
-//     stagger: .3
-// })
+tl.from('.logo span', {
+    y: -60,
+    duration: 1,
+    ease: "power2.inOut",
+    stagger: .3
+})
 
-// tl.from('.links a', {
-//     y: -60,
-//     duration: .7,
-//     ease: "power2.inOut",
-//     stagger: .2
-// })
+tl.from('.links a', {
+    y: -60,
+    duration: .7,
+    ease: "power2.inOut",
+    stagger: .2
+})
 
+tl.from('.text-container h2', {
+    x: -30,
+    opacity: 0,
+    ease: "power1.inOut",
+    duration: .3
+}, "first")
+
+tl.from('.text-container p', {
+    x: 30,
+    opacity: 0,
+    ease: "power1.inOut",
+    duration: .3
+}, "first")
+
+
+tl.from('.hero-right img', {
+    opacity: 0,
+    x: 30,
+    duration: .5
+}, "first")
+
+tl.to('.looping-text', {
+    transform: 'translateX(-200%)',
+    duration: 3,
+    repeat: -1,
+    ease: 'none'
+})
